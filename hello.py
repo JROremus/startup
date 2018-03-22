b746d8fccd2dd7487cee9205042168221d7b83a4
Unknown <jroremus@gmail.com>
2018-03-22 13:18:33 -0500

    added the base file and altered index

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index")
+@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
